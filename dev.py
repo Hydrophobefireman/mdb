0d49afdf13087f5d44af39be7ec475345508c2fb
me <bhavesh.pareek36@gmail.com>
2020-08-07 00:11:12 +0530

    heroku

--- dev.py
+++
@@ -1,3 +0,0 @@
-if __name__=="__main__":
-	from api.query import app
-	app.run(debug=True)
--- frontend/injectables/preloadPaths.js
+++ frontend/injectables/preloadPaths.js
@@ -4,7 +4,7 @@ const code = function () {
   var paths = window.__mdbApp.preloadPaths;
   var host = location.host.includes("localhost")
     ? "http://localhost:5000"
-    : location.origin;
+    : "https://mdpy.herokuapp.com";
   window.__mdbApp.config["app.apiHost"] = host;
   for (key in paths) {
     var _ = paths[key];
