f98d5f26749b602c2adb634b7d8dfeef342687ae
me <bhavesh.pareek36@gmail.com>
2020-08-07 20:36:10 +0530

    remove randomized taglines

--- frontend/static/components/MovieDetailsComponent/Taglines.js
+++ frontend/static/components/MovieDetailsComponent/Taglines.js
@@ -1,21 +1,21 @@
-import { h, useState, useEffect, useCallback } from "@hydrophobefireman/ui-lib";
+import { h, useState, useCallback } from "@hydrophobefireman/ui-lib";
 export function Taglines({ taglines }) {
-  const [current, setCurrent] = useState(null);
-  const getRandom = useCallback(() => {
-    let next;
-    while (
-      (next = taglines[Math.floor(Math.random() * taglines.length)]) == current
-    ) {}
-    setCurrent(next);
-  }, [taglines, current]);
-  useEffect(getRandom, [taglines]);
+  const [index, setIndex] = useState(0);
+  const getNext = useCallback(() => {
+    if (!taglines) return;
+    let next = index + 1;
+    const taglinesLength = taglines.length;
+    if (taglinesLength === next) next = 0;
+    setIndex(next);
+  }, [taglines, index]);
+
   return h(
     "div",
     {
       class: "fancy-text pointer",
       style: { fontSize: "4rem" },
-      onClick: getRandom,
+      onClick: getNext,
     },
-    current
+    taglines && taglines[index]
   );
 }
